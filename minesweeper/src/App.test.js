8f6ec0dec40b5088897703915bd3dc7f28c603c3
wballard7 <wballard7@gmail.com>
2024-08-05 17:57:46 -0400

    just confirming saves

--- minesweeper/src/App.test.js
+++ minesweeper/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a Minesweeper title and a 10x10 minesweeper grid', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const pageTitle = screen.getByText('Minesweeper');
   expect(linkElement).toBeInTheDocument();
 });
